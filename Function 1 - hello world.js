8407ecfa187401a587ea71f7137bc7322cb833a2
ilyastrijakov <pasv.qa.student5@gmail.com>
2020-05-08 18:12:56 -0400

    Added solution to Function 1 - hello world task;

---
+++ Function 1 - hello world.js
@@ -0,0 +1,14 @@
+Function 1 - hello world
+
+
+https://www.codewars.com/kata/523b4ff7adca849afe000035
+
+
+
+
+    function greet(){
+        const name = "hello world!";
+        console.log(name);
+        return "hello world!";
+    }
+
