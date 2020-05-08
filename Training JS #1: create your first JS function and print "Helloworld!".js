de20bcf229e69e31e581cab4f4540ed8dabd5e46
ilyastrijakov <pasv.qa.student5@gmail.com>
2020-05-08 18:15:57 -0400

    Added solution to Training JS #1: create your first JS function a task;

---
+++ Training JS #1: create your first JS function and print "Helloworld!".js
@@ -0,0 +1,10 @@
+Training JS #1: create your first JS function a
+
+
+
+
+https://www.codewars.c// om/kata/571ec274b1c8d4a61c0000c8
+    function helloWorld(){
+        var str = "Hello World!";
+        console.log(str);
+    }
