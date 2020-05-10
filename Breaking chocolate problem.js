c383f746b453a283cc77f46d06587c56183ef38e
ilyastrijakov <pasv.qa.student5@gmail.com>
2020-05-09 22:39:55 -0400

    Added solution to a Breaking chocolate problem task;

---
+++ Breaking chocolate problem.js
@@ -0,0 +1,12 @@
+Breaking chocolate problem
+
+
+https://www.codewars.com/kata/534ea96ebb17181947000ada
+
+
+
+    function breakChocolate(n,m) {
+        if(n > 0 && m > 0) return ( n * m) -1;
+        else return 0;
+
+    }
