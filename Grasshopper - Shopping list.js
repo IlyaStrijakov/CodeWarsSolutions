b1e2972927678c0e3a39bff9ad384ac017ef441f
ilyastrijakov <pasv.qa.student5@gmail.com>
2020-05-15 22:26:07 -0400

    Added solution to a Beginner Grasshopper - Shopping listtask;

---
+++ Grasshopper - Shopping list.js
@@ -0,0 +1,10 @@
+Grasshopper - Shopping list
+
+https://www.codewars.com/kata/560c31275c39c481c4000022
+
+
+    let sandwiches = 4
+let salads =  6
+let wraps = 5
+let frenchFries =  10
+let totalPrice = (salads * 7)+ (sandwiches  * 8)+ (wraps * 6.5) + (frenchFries * 1.2);
