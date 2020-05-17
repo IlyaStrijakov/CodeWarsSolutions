a8dc7f8b3d7ca0e18dc0ee73abd527e226d10670
ilyastrijakov <pasv.qa.student5@gmail.com>
2020-05-16 23:55:58 -0400

    Added solution to a Grasshopper - Make change task;

---
+++ Grasshopper - Make change.js
@@ -0,0 +1,11 @@
+Grasshopper - Make change
+
+
+https:www.codewars.com/kata/560dab9f8b50f89fd6000070
+
+
+ let money = 10;
+let candy = 1.42;
+let chips = 2.40;
+let soda  = 1.00;
+let change = 10 - (candy + chips + soda);
