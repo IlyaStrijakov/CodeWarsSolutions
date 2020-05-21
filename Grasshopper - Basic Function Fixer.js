85b531f596d0368a27bd90c71674f08624548bdb
ilyastrijakov <pasv.qa.student5@gmail.com>
2020-05-21 19:00:57 -0400

    Added solution to a Grasshopper - Basic Function Fixer task;

---
+++ Grasshopper - Basic Function Fixer.js
@@ -0,0 +1,13 @@
+Grasshopper - Basic Function Fixer
+
+
+
+https://www.codewars.com/kata/56200d610758762fb0000002
+
+
+
+
+    function addFive(num) {
+        var total = num = num + 5
+        return num
+    }
