a46efb0a8314907faac29e800a23b103288c3e67
ilyastrijakov <pasv.qa.student5@gmail.com>
2020-05-26 22:40:35 -0400

    Added solution to a For Twins: 2. Math operations task;

---
+++ For Twins: 2. Math operations.js
@@ -0,0 +1,8 @@
+For Twins: 2. Math operations
+
+https://www.codewars.com/kata/59c287b16bddd291c700009a
+
+
+    function iceBrickVolume(radius, bottleLength, rimLength) {
+        return ((bottleLength-rimLength)*(radius*2)*radius);
+    }
